cc33aa013683aa02b3877dc219ddf750aa8f9777
John Jeffery <jjeffery@sp.com.au>
2019-08-14 19:01:16 +1000

    Add Log function, List.Log method

--- example_test.go
+++ example_test.go
@@ -2,10 +2,20 @@ package kv_test
 
 import (
 	"context"
-	log "fmt"
+	"fmt"
+	"strings"
 
 	"github.com/jjeffery/kv"
 )
+
+func init() {
+	// Use fmt.Println for logging messages so that Output works
+	// in example test cases.
+	kv.LogOutput = func(calldepth int, s string) error {
+		_, err := fmt.Println(strings.TrimSpace(s))
+		return err
+	}
+}
 
 func ExampleContext() {
 	// start with a context
@@ -16,7 +26,7 @@ func ExampleContext() {
 
 	// ... later on log a message ...
 
-	log.Println("permission denied", kv.From(ctx).With("user", "alice"))
+	fmt.Println("permission denied", kv.From(ctx).With("user", "alice"))
 
 	// Output:
 	// permission denied user=alice method=get url="/api/widgets"
@@ -30,13 +40,38 @@ func ExampleError() {
 
 	// create an error and log it
 	err := kv.NewError("permission denied").With("user", user)
-	log.Println(err)
+	fmt.Println(err)
 
 	// alternatively, wrap an existing error
 	err = kv.Wrap(err, "cannot open file").With("file", "/etc/passwd")
-	log.Println(err)
+	fmt.Println(err)
 
 	// Output:
 	// permission denied user=alice
 	// cannot open file: permission denied file="/etc/passwd" user=alice
+}
+
+func ExampleLog() {
+	kv.Log("message 1")
+	kv.Log("message 2", kv.With("a", 1))
+
+	kv := kv.With("p", "q")
+
+	kv.Log("message 3")
+	kv.Log("message 4", kv.With("a", 1, "b", 2))
+
+	ctx := kv.From(context.Background()).With("c1", 100, "c2", 200)
+
+	kv.Log(ctx, "message 5")
+	kv.Log("message 6:", ctx, "can be in any order")
+	kv.Log(ctx, "message 7", kv.With("a", 1, "b", 2))
+
+	// Output:
+	// message 1
+	// message 2 a=1
+	// message 3 p=q
+	// message 4 p=q a=1 b=2
+	// message 5 p=q c1=100 c2=200
+	// message 6: can be in any order p=q c1=100 c2=200
+	// message 7 p=q a=1 b=2 c1=100 c2=200
 }
